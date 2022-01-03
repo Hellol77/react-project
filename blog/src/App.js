d8c0d0cac18a47e47e32d73d2028c308fd901e0f
Hellol77 <dhe7700@naver.com>
2022-01-04 04:24:14 +0900

    UI

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable */
 import React, { useState } from "react";
 import "./App.css";
 
@@ -8,19 +8,19 @@ function App() {
     "강남 우동 맛집",
     "안녕 넌 누구니",
   ]);
-  let [따봉,따봉변경]=useState(0);
-  function 제목바꾸기(){
+  let [따봉, 따봉변경] = useState(0);
+  let [modal, modal변경] = useState(false);
+  let [누른제목,누른제목변경] = useState(0);
+  let [입력값, 입력값변경] = useState('');
+  function 제목바꾸기() {
     var newArray = [...글제목];
-    newArray[0]='여자 코트 추천';
+    newArray[0] = "여자 코트 추천";
     글제목변경(newArray);
-  
   }
-  function 가나다정렬(){
+  function 가나다정렬() {
     var newArray = [...글제목];
     newArray.sort();
     글제목변경(newArray);
-  
-  
   }
   return (
     <div className="App">
@@ -29,21 +29,45 @@ function App() {
       </div>
       <button onClick={제목바꾸기}>버튼</button>
       <button onClick={가나다정렬}>정렬버튼</button>
-      <div className="list">
-        <h3>{글제목[0]} <span onClick={()=>{따봉변경(따봉 +1)}}>💖</span>{따봉}</h3>
-        <p>2022-01-03</p>
-        <hr />
+
+      {글제목.map((글,i) => {
+        return (
+          <div className="list" key={i}>
+            <h3 onClick={()=>{누른제목변경(i)}}>{글}<span onClick={()=>{따봉변경(따봉 +1)}}>💖</span>{따봉}</h3>
+            <p>2022-01-05</p>
+            <hr />
+          </div>
+        );
+      })}
+      {/* <button onClick={()=>{누른제목변경(0)}}>버튼1</button>
+      <button onClick={()=>{누른제목변경(1)}}>버튼2</button>
+      <button onClick={()=>{누른제목변경(2)}}>버튼3</button> */}
+      {/* <input onChange={ (e)=>{입력값변경(e.target.value) }}/> */}
+      
+      
+      <div className='publish'>
+        <input onChange={(e)=>{입력값변경(e.target.value)}}/> 
+        <button onClick={()=>{글제목변경(글제목.concat(입력값))}}>저장</button>
       </div>
-      <div className="list">
-        <h3>{글제목[1]}</h3>
-        <p>2022-01-04</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h3>{글제목[2]}</h3>
-        <p>2022-01-05</p>
-        <hr />
-      </div>
+      
+      <button
+        onClick={() => {
+          modal변경(!modal);
+        }}
+      >
+        modal버튼
+      </button>
+      {modal === true ? <Modal 글제목={글제목} 누른제목={누른제목}/> : null}
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className="modal">
+      <h2>{props.글제목[props.누른제목]}</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
