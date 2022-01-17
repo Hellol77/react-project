b561a8fdc225ee415911ee275e87c568d07f9904
Hellol77 <dhe7700@naver.com>
2022-01-17 18:36:41 +0900

    add firestore

--- twitter/src/routes/Home.js
+++ twitter/src/routes/Home.js
@@ -1,5 +1,38 @@
 /* eslint-disable*/
-import React from "react";
+import { dbService } from "fBase";
+import { addDoc, collection } from "firebase/firestore";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const docRef = await addDoc(collection(dbService, "tweets"), {
+      tweet,
+      createdAt: Date.now(),
+    });
+    setTweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="what's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  );
+};
+
 export default Home;
